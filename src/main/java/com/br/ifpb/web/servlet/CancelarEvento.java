6f7cb0553f29c4946b9c72937cad1543e2a21549
EmanuelBatista <emanuelbatista2011@gmail.com>
2015-04-07 21:01:55 -0300

    Atualizando Bugs 3

--- src/main/java/com/br/ifpb/web/servlet/CancelarEvento.java
+++ src/main/java/com/br/ifpb/web/servlet/CancelarEvento.java
@@ -26,50 +26,21 @@ public class CancelarEvento extends HttpServlet {
         try {
             GerenciarEvento gerenciarEvento = new GerenciarEvento();
             gerenciarEvento.mudarStatus(idEvento, Evento.STATUS_CANCELADO);
-            response.sendRedirect("eventos");
         } catch (PersistenciaException ex) {
             Logger.getLogger(CancelarEvento.class.getName()).log(Level.SEVERE, null, ex);
         }
         response.sendRedirect("eventos");
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
