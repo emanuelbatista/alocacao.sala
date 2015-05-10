e120c4bab86c8968e89f57750fe3b75e2c8cd434
emanuelbatista <emanuelbatista2011@gmail.com>
2015-05-10 20:26:38 -0300

    alterando o arquivo do tivis ci

--- libs/mvn-install-libs.sh
+++ libs/mvn-install-libs.sh
@@ -1,5 +1,1 @@
-# Esse script instala a dependência do SwingR no repositório Maven
-# Solução temporária enquanto não temos o código-fonte do módulo
-script_path=$(readlink -f "$0")
-path=$(dirname $script_path)
-mvn install:install-file -Dfile="$path/swingr.jar" -DgroupId=br.com.recursive.swingr -DartifactId=swingr -Dversion=1.0 -Dpackaging=jar
+mvn install -DskipTests=true
