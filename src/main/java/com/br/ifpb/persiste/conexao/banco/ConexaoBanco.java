3dd75290d6873e207d559b68df19b27cdf8a6a0a
emanuelbatista <emanuelbatista2011@gmail.com>
2015-05-14 21:03:55 -0300

    modificando configuração do banco para o heroku

--- src/main/java/com/br/ifpb/persiste/conexao/banco/ConexaoBanco.java
+++ src/main/java/com/br/ifpb/persiste/conexao/banco/ConexaoBanco.java
@@ -1,27 +1,48 @@
 package com.br.ifpb.persiste.conexao.banco;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
  * Classe que representa a conexão com o banco de dados
- * @author Emanuel
+ *
+ * @author emanuel
+ * @author DouglasGabriel
  */
 public class ConexaoBanco {
-    
+
     /**
-     * Esse método retorna a conexão com o banco de dados 
+     * Esse método retorna a conexão com o banco de dados
+     *
      * @return {@link Connection}
      * @throws ClassNotFoundException
-     * @throws SQLException 
+     * @throws SQLException
      */
-    public static  Connection getConexao() throws ClassNotFoundException, SQLException{
-        PropriedadeBanco propriedadeBanco=PropriedadeBanco.getInstance();
-        Class.forName(propriedadeBanco.getString("drive"));
-        String url=propriedadeBanco.getString("url");
-        String user=propriedadeBanco.getString("user");
-        String password=propriedadeBanco.getString("password");
-        return DriverManager.getConnection(url, user, password);
+    public static Connection getConexao() throws ClassNotFoundException, SQLException{
+        // Forma de conectar-se ao banco de dados Heroku
+        String username, password;
+        try {
+            Connection con = null;
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+            username = dbUri.getUserInfo().split(":")[0];
+            password = dbUri.getUserInfo().split(":")[1];
+            int port = dbUri.getPort();
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
+            if ((con = DriverManager.getConnection(dbUrl, username, password)) != null) {
+                return con;
+            }
+        } catch (Exception e) {
+            // Forma de conectar-se ao banco de dados local
+            PropriedadeBanco propriedadeBanco = PropriedadeBanco.getInstance();
+            Class.forName(propriedadeBanco.getString("drive"));
+            String url = propriedadeBanco.getString("url");
+            username = propriedadeBanco.getString("user");
+            password = propriedadeBanco.getString("password");
+            return DriverManager.getConnection(url, username, password);
+        }
+        return null;
     }
 }
